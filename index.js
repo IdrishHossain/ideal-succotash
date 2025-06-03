4a774b4177717089339aaf9bda9a5bb8f97c85c8
IdrishHossain <hossainenv28@gmail.com>
2025-06-03 00:10:52 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+function donate() {
+    alert("Thank you for your support! Visit our donation page to contribute.");
+    window.location.href = "https://www.paypal.com/donate";  // Replace with actual donation link
+}
